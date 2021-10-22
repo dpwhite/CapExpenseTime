c3ab0acd669e12c7c0688cefc58f92786cbfe167
Durward White <dwhite1@newpark.com>
2021-10-22 15:48:54 -0500

    added projects

---
+++ CapExpenseTime.Api/ProjectViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapExpenseTime.API
+{
+    public class ProjectViewModel
+    {
+        public string Name { get; set; }
+        public string ProjectType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Description { get; set; }
+        public List<EmployeeViewModel> Employees { get; set; }
+    }
+}
--- CapExpenseTime.Api/Startup.cs
+++ CapExpenseTime.Api/Startup.cs
@@ -1,24 +1,22 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using CapExpenseTime.Data;
 
-namespace CapExpenseTime.Web
+namespace CapExpenseTime.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IWebHostEnvironment env)
-        {
-            Configuration = configuration;
-            this.env = env;
-        }
-
-        public IConfiguration Configuration { get; }
-        public IWebHostEnvironment env { get; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
@@ -39,8 +37,11 @@ namespace CapExpenseTime.Web
             else
             {
                 app.UseExceptionHandler("/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
             }
 
+            app.UseHttpsRedirection();
             app.UseStaticFiles();
             if (!env.IsDevelopment())
             {
@@ -54,6 +55,10 @@ namespace CapExpenseTime.Web
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller}/{action=Index}/{id?}");
+                //endpoints.MapGet("/", async context =>
+                //{
+                //    await context.Response.WriteAsync("Hello World!");
+                //});
             });
 
             app.UseSpa(spa =>
