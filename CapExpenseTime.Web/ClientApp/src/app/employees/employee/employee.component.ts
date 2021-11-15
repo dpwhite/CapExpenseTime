4123734de92eeee7dfca315d7248eec2aa838482
Durward White <dwhite1@newpark.com>
2021-11-15 17:36:14 -0600

    added employee component

---
+++ CapExpenseTime.Web/ClientApp/src/app/employees/employee/employee.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { ProjectEmployee, EmployeeService } from '../index';
+
+@Component({
+  selector: 'employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.scss']
+})
+
+export class EmpoloyeeComponent implements OnInit {
+
+  ngOnInit() {}
+}
--- CapExpenseTime.Web/ClientApp/src/app/employees/employees-list/employees-list.component.ts
+++ CapExpenseTime.Web/ClientApp/src/app/employees/employees-list/employees-list.component.ts
@@ -3,27 +3,6 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ProjectEmployee, EmployeeService } from '../index';
 
 
-export interface PeriodicElement {
-  first: string;
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, first: 'durward', name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, first: 'durward', name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, first: 'durward', name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, first: 'durward', name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, first: 'durward', name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, first: 'durward', name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, first: 'durward', name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, first: 'durward', name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, first: 'durward', name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, first: 'durward', name: 'Neon', weight: 20.1797, symbol: 'Ne' },
-];
-
 @Component({
   selector: 'app-employees-list',
   templateUrl: './employees-list.component.html',
@@ -31,8 +10,6 @@ const ELEMENT_DATA: PeriodicElement[] = [
 })
 export class EmployeesListComponent implements OnInit {
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  dataSource = ELEMENT_DATA;
   employeeProjects: ProjectEmployee[] = [];
   columnHeaders: string[] =
     [
