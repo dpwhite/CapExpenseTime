250e8eb3870e0e57fa1e24b1e56b3413e9857f71
Durward White <dwhite1@newpark.com>
2021-11-22 09:12:19 -0600

    updated employee list to edit employee

--- CapExpenseTime.Web/ClientApp/src/app/app.module.ts
+++ CapExpenseTime.Web/ClientApp/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
@@ -32,6 +32,7 @@ import { EmployeesListComponent, EmployeeComponent } from './employees/index';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot(appRoutes),
     //RouterModule.forRoot([
     //  { path: '', component: HomeComponent, pathMatch: 'full' },
--- CapExpenseTime.Web/ClientApp/src/app/employees/employees-list/employees-list.component.ts
+++ CapExpenseTime.Web/ClientApp/src/app/employees/employees-list/employees-list.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Params, Router } from '@angular/router';
 import { ProjectService, Project } from '../../projects/index';
 import { ProjectEmployee } from '../employee.model';
 import { EmployeeService } from '../employee.service';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-employees-list',
@@ -27,13 +28,28 @@ export class EmployeesListComponent implements OnInit {
   tableData: any;
   clickedRows = new Set<ProjectEmployee>();
   projectName: string;
+  rowSelected: boolean = false;
+  myForm: FormGroup;
 
-  constructor(private router: Router, private route: ActivatedRoute, private service: EmployeeService, private projectService: ProjectService ) { }
+  constructor(private router: Router, private route: ActivatedRoute, private service: EmployeeService, private projectService: ProjectService, private fb: FormBuilder ) { }
 
   ngOnInit() {
     this.projectId = this.route.snapshot.paramMap.get("id") || '';
     this.yearMonth = this.route.snapshot.paramMap.get("yearMonth");
     this.employeeProject = new ProjectEmployee();
+
+    this.myForm = this.fb.group(
+      {
+        name: [''],
+        afe: [''],
+        projectManagement: [''],
+        gapAnalysis: [''],
+        solutionDesign: [''],
+        dataConversion: [''],
+        testing: [''],
+        training: ['']
+      }
+    );
 
     console.log('project id: ', this.projectId);
     this.projectService.getProject(this.projectId).subscribe(
@@ -59,8 +75,20 @@ export class EmployeesListComponent implements OnInit {
 
   selectRow(row: any) {
     this.clickedRows.clear();
-    this.clickedRows.add(row);
-    this.employeeProject = row; 
+    if (this.myForm.controls.name.value != row.name) {
+      this.myForm.controls.name.setValue(row.name);
+      this.clickedRows.add(row);
+      this.employeeProject = row;
+      this.rowSelected = true;
+    }
+    else {
+      this.rowSelected = false;
+     
+    }
+  }
+
+  onSubmit(): void {
+    console.log('submit button clicked');
   }
 
 }
